8be9b1d26025d5de19796c82aff8c997a3fad1c8
TDibabacodes <126144468+TDibabacodes@users.noreply.github.com>
2024-07-12 10:54:09 +0200

    Column type changes

--- ExpenseTrackerApp/Models/Category.cs
+++ ExpenseTrackerApp/Models/Category.cs
@@ -18,6 +18,21 @@ namespace ExpenseTrackerApp.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
 
+        [NotMapped]
+        public string? TitleWithIcon {
+
+
+            get
+            {
+                return this.Title + " " + this.Icon;
+            }
+        
+        
+        
+        
+        }
+
+
 
 
 
