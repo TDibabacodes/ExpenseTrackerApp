68d120fbc26d7f3fd061a6144c384df5e9ef7a32
TDibabacodes <126144468+TDibabacodes@users.noreply.github.com>
2024-07-23 11:18:39 +0200

    Detailed Action Method removed

--- ExpenseTrackerApp/Controllers/CategoryController.cs
+++ ExpenseTrackerApp/Controllers/CategoryController.cs
@@ -24,23 +24,7 @@ namespace ExpenseTrackerApp.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var category = await _context.Categories
-        //        .FirstOrDefaultAsync(m => m.CategoryId == id);
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(category);
-        //}
+        
 
         // GET: Category/AddorEdit
         public IActionResult AddorEdit(int id=0)
@@ -86,24 +70,6 @@ namespace ExpenseTrackerApp.Controllers
 
      
 
-        // GET: Category/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var category = await _context.Categories
-        //        .FirstOrDefaultAsync(m => m.CategoryId == id);
-        //    if (category == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(category);
-        //}
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -119,9 +85,6 @@ namespace ExpenseTrackerApp.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        //private bool CategoryExists(int id)
-        //{
-        //    return _context.Categories.Any(e => e.CategoryId == id);
-        //}
+      
     }
 }
